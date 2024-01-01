b40645504bcedb9471706e460399d5b17b5e0227
Alex Whitehead-Smith <alex.me.smith@gmail.com>
2024-01-01 14:23:10 +0000

    Move site content into src/ folder

--- .eleventy.js
+++ .eleventy.js
@@ -2,5 +2,11 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("css");
   return {
     passthroughFileCopy: true,
+    dir: {
+      input: "src",
+      includes: "_includes",
+      data: "_data",
+      output: "_site",
+    },
   };
 };
