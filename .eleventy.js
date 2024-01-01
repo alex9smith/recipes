ffbd646d126a10f4de1dd038f2938ea897eb938a
Alex Whitehead-Smith <alex.me.smith@gmail.com>
2024-01-01 20:45:40 +0000

    Add robots.txt
    Allows all useragents to crawl the whole site

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/css");
+  eleventyConfig.addPassthroughCopy("src/robots.txt");
   return {
     passthroughFileCopy: true,
     dir: {
